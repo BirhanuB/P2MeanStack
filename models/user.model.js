3e7424049a29f7ae8cf5a5cf33eb64902cd93476
BirhanuB <55416464+BirhanuB@users.noreply.github.com>
2022-01-13 04:06:32 -0500

    added user parameters with backend validation & error handling

--- models/user.model.js
+++ models/user.model.js
@@ -4,16 +4,36 @@ const bcrypt = require('bcryptjs');
 var userSchema = new mongoose.Schema({
     fullName: {
         type: String,
+        required: 'Full name can\'t be empty'
     },
     email: {
         type: String,
-
+        required: 'Email can\'t be empty',
+        unique: true
     },
     password: {
         type: String,
-
+        required: 'Password can\'t be empty',
+        minlength : [4,'Password must be at least 4 characters long']
     },
     saltSecret: String
+});
+
+// Custom validation for email
+userSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail');
+
+// Events (before the save operation)
+userSchema.pre('save', function (next) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            this.password = hash;
+            this.saltSecret = salt;
+            next(); // proceed to next operations
+        });
+    });
 });
 
 mongoose.model('User', userSchema);
